e1629758716ee954f4fc42a62486329dbff807d7
NgocNhan43 <nguyenngocnhan442002@gmail.com>
2024-01-10 08:09:53 +0700

    Nodejs

---
+++ node-express/routes/leaderRouter.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        next();
+    })
+    .get((req, res, next) => {
+        res.end('Will send all the leaders to you!');
+    })
+    .post((req, res, next) => {
+        res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /leaders');
+    })
+    .delete((req, res, next) => {
+        res.end('Delete all leaders');
+    });
+
+leaderRouter.route('/:leaderId')
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        next();
+    })
+    .get((req, res, next) => {
+        res.end('Will send all leaders to you!');
+    })
+    .post((req, res, next) => {
+        res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /dishes');
+    })
+    .delete((req, res, next) => {
+        res.end('Will delete all leaders');
+    });
+
+module.exports = leaderRouter;
